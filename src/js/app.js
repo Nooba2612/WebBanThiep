bb8b4306bac5860abdaf0a8c92193d0072541613
Nooba <dpnguyen261204@gmail.com>
2024-01-17 18:16:26 +0700

    :fire: add shopping cart features

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
 /* Variables */
 const $ = document.querySelector.bind(document);
 
+let cartItemQuantity = 0;
 const seacrhBoxInput = $(".navbar--item__search-box input");
 
 /* Functions */
+const handleNullFeature = () => {
+    alert("Chức năng đang được chúng tôi phát triển");
+};
+
 const handleSubmitForm = (e) => {
     e.preventDefault();
     console.log(seacrhBoxInput.value);
@@ -17,6 +22,43 @@ const handleEnterPressForm = (e) => {
     }
 };
 
-const handleNullFeature = () => {
-    alert("Chức năng đang được chúng tôi phát triển");
+const handleSearchSuggestClick = (e) => {
+    seacrhBoxInput.value = e.target.innerText;
+    console.log(seacrhBoxInput.value);
 };
+
+const handleClearInput = () => {
+    seacrhBoxInput.value = "";
+};
+
+const handleShoppingCart = () => {
+    const shoppingCart = $(".navbar--item__cart");
+    const emptyCart = $(".navbar--item__cart .empty-cart");
+    const productsListCart = $(".navbar--item__cart .products-list");
+    const cartProductsNumber = $(".navbar--item__cart .items-quantity");
+
+    cartProductsNumber.innerText = cartItemQuantity;
+
+    shoppingCart.addEventListener("mouseenter", () => {
+        if (cartItemQuantity === 0) {
+            emptyCart.style.display = "block";
+        } else {
+            productsListCart.style.display = "block";
+        }
+    });
+
+    shoppingCart.addEventListener("mouseleave", () => {
+        if (cartItemQuantity === 0) {
+            emptyCart.style.display = "none";
+            return;
+        }
+
+        productsListCart.style.display = "none";
+    });
+};
+
+const App = () => {
+    localStorage.setItem("cartItemQuantity", cartItemQuantity);
+    handleShoppingCart();
+};
+App();
